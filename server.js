8fe78f3c74990ab6d533bc3df5ce43fa47b0517c
Victor Aguirre <victoraguirre@Victors-MacBook-Pro.local>
2026-03-24 00:01:15 -0700

    add html frontend

--- server.js
+++ server.js
@@ -1,17 +1,11 @@
 const express = require("express");
 const cors = require("cors");
+const path = require("path");
 
 const app = express();
-
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
-  res.header("Access-Control-Allow-Headers", "Content-Type");
-  if (req.method === "OPTIONS") return res.sendStatus(200);
-  next();
-});
-
+app.use(cors());
 app.use(express.json({ limit: "20mb" }));
+app.use(express.static(path.join(__dirname)));
 
 app.post("/scan", async (req, res) => {
   try {
@@ -43,5 +37,5 @@ app.post("/scan", async (req, res) => {
   }
 });
 
-app.get("/", (req, res) => res.send("Valet Scanner API running"));
+app.get("/", (req, res) => res.sendFile(path.join(__dirname, "index.html")));
 app.listen(process.env.PORT || 3001, () => console.log("Server running"));
